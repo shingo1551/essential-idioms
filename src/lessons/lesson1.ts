39cac1c702b33f85b7f2e5a12e0017174ea18a89
Shingo Tanikawa <dubai2k@gmail.com>
2021-04-22 15:03:47 +0900

    1

--- src/lessons/lesson1.ts
+++ src/lessons/lesson1.ts
@@ -66,7 +66,20 @@ export const lesson1: Lesson = [
       ja: '脱ぐ',
     },
     note: 'remove',
-    phrases: [],
+    phrases: [
+      {
+        en: 'John took off his hat as he entered the room.',
+        ja: 'ジョンは部屋に入りながら帽子を脱いだ。',
+      },
+      {
+        en: 'Is Helen taking off her coast because it is too hot in the room?',
+        ja: '',
+      },
+      {
+        en: 'Take your sweater off in this warm room.',
+        ja: 'この部屋は暖かいからセーターは脱ぎなさい。',
+      },
+    ],
   },
   {
     idiom: {
@@ -74,7 +87,16 @@ export const lesson1: Lesson = [
       ja: '電話をかける',
     },
     note: 'telephone',
-    phrases: [],
+    phrases: [
+      {
+        en: "I forgot to call up Mr. Jones yesterday, although I promised to call him exactly at 3 o'clock.",
+        ja: '昨日, 3時きっかりに電話をかけると約束したのにジョーンズ氏に電話するを忘れてしまった。',
+      },
+      {
+        en: "Call me Up tomorrow, Jane: we'll arrange to have lunch together.",
+        ja: 'ジェーン, 明日電話してくれるかい。一緒に昼食を食べる打ち合わせをするから。',
+      },
+    ],
   },
   {
     idiom: {
@@ -82,6 +104,67 @@ export const lesson1: Lesson = [
       ja: '(あかり,ラジオなど)をつける',
     },
     note: 'start, begin',
+    phrases: [
+      {
+        en: 'Please turn on the light; this room is dark',
+        ja: '電気をつけてください。この部屋はくらいですね。',
+      },
+      {
+        en: 'Someone turned on the radio while we were out.',
+        ja: '私たちが留守の間に誰かがラジオをつけた。',
+      },
+      {
+        en: 'Do you know who turned it on?',
+        ja: '誰がつけたのか知っていますか。',
+      },
+    ],
+  },
+  {
+    idiom: {
+      en: 'turn ~ off',
+      ja: '(あかり,ラジオなど)を消す, (電源を)切る',
+    },
+    note: 'stop, terminate, extinguish',
+    phrases: [],
+  },
+  {
+    idiom: {
+      en: 'right away',
+      ja: 'ただちに',
+    },
+    note: 'immediately, very soon',
+    phrases: [],
+  },
+  {
+    idiom: {
+      en: 'pick ~ up',
+      ja: '拾い[取り]上げる(特に指を使って)',
+    },
+    note: 'take',
+    phrases: [],
+  },
+  {
+    idiom: {
+      en: 'at once',
+      ja: 'ただちに, すぐに',
+    },
+    note: 'immediately, very soon, right away',
+    phrases: [],
+  },
+  {
+    idiom: {
+      en: 'get ~ up',
+      ja: '起きる, 立ち上がる, 起こす',
+    },
+    note: undefined,
+    phrases: [],
+  },
+  {
+    idiom: {
+      en: 'at first',
+      ja: 'はじめは',
+    },
+    note: 'originally, in the first instance',
     phrases: [],
   },
 ];
